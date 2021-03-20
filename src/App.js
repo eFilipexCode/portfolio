13e5b67f4ca4ccdb073c7f14671ba6c006ee97b3
Emanuel Filipe <efilipecode@gmail.com>
2021-03-20 19:27:22 -0300

    Images

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import Header from './components/Header/Header.jsx';
+import Header from './components/Header/Header';
+import Showcase from './components/Showcase/Showcase';
+import ShowCaseAbout from './components/ShowCaseAbout/ShowCaseAbout';
+import SectionSelector from './components/SectionSelector/SectionSelector';
+import Topic from './components/Topic/Topic';
+import { WebsitesContextProvider } from './contexts/WebsitesContext';
 
 function App() {
   return (
-    <div className="App">
+    <WebsitesContextProvider>
       <Header />
-    </div>
+      <Topic title="About me" description="Who's efilipex? 🤔" />
+      <ShowCaseAbout title="Emanuel Filipe" description="Hello! Super glad you're here 😁. I'm Emanuel Filipe - or also called efilipex - and I'm a frontend development student, seeking new knowledges and skills! I'm currently learning ReactJs, React Native, Node, Svelte, Typescript and Python." />
+      <Topic title="Projects" description="Here are some of the projects I build on my own! Let's check it out."/>
+      <SectionSelector />
+      <Showcase />
+      <Topic title="Contact" description="Let's keep in touch!" />
+    </WebsitesContextProvider>
   );
 }
 
