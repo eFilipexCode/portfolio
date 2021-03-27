8948c840afbaf276f2dcd8f36c723982bf6b990e
Emanuel Filipe <efilipecode@gmail.com>
2021-03-27 19:25:55 -0300

    Add code block

--- src/components/ShowCaseAbout/ShowCaseAbout.jsx
+++ src/components/ShowCaseAbout/ShowCaseAbout.jsx
@@ -1,14 +1,25 @@
 import './ShowCaseAbout.css';
+import { CopyBlock, dracula } from 'react-code-blocks';
 
 export default function ShowcaseAbout({ title, description }) {
+    const code = `const me = {
+    name: "Emanuel Filipe",
+    age: 18,
+    country: "Brazil",
+    email: "efilipecode@gmail.com",
+    github: "https://github.com/eFilipexCode",
+    linkedIn: "https://www.linkedin.com/in/efilipex",
+    loves_coding: true
+};
+`;
     return (
         <div className="showcaseAbout-container">
             <div className="showcase-texts">
                 <h1>{title}</h1>
                 <p className="description">{description}</p>
             </div>
-            <div className="showcase-image">
-                <img src='/emanuel.PNG' alt="emanuel.json" />
+            <div className="showcase-code">
+                <CopyBlock theme={dracula} text={code} language="jsx" showLineNumbers />
             </div>
         </div>
     );
