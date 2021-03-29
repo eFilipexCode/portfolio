3dce49d6e000510a026f001fc85ab447282ec76e
Emanuel Filipe <efilipecode@gmail.com>
2021-03-29 11:34:42 -0300

    Update ContactTab.jsx

--- src/components/ContactTab/ContactTab.jsx
+++ src/components/ContactTab/ContactTab.jsx
@@ -5,25 +5,25 @@ import { FiGithub, FiLinkedin, FiMail, FiCoffee } from 'react-icons/fi';
 export default function ContactTab() {
     return (
         <div className="contact-container">
-            <a href="https://github.com/eFilipexCode">
+            <a href="https://github.com/eFilipexCode" target="_blank">
                 <div className="contact-option">
                     <FiGithub size={60} color="#151515" />
                     <p>Follow me on Github 💻</p>
                 </div>
             </a>
-            <a href="https://www.linkedin.com/in/emanuel-filipe-1599b41a6/">
+            <a href="https://www.linkedin.com/in/emanuel-filipe-1599b41a6/" target="_blank">
                 <div className="contact-option">
                     <FiLinkedin size={60} color="#151515" />
                     <p>Visit my LinkedIn 🔗</p>
                 </div>
             </a>
-            <a href="mailto:efilipecode@gmail.com?Subject=Contacting via website">
+            <a href="mailto:efilipecode@gmail.com?Subject=Contacting via website" target="_blank">
                 <div className="contact-option">
                     <FiMail size={60} color="#151515" />
                     <p>Email me 📩</p>
                 </div>
             </a>
-            <a href="https://www.buymeacoffee.com/dimeboyu">
+            <a href="https://www.buymeacoffee.com/dimeboyu" target="_blank">
                 <div className="contact-option">
                     <FiCoffee size={60} color="#151515" />
                     <p>Would you buy me a coffee? ☕</p>
