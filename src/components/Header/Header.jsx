1ed56938c102dc378b3f6c2dddf350f313e46ceb
Emanuel Filipe <efilipecode@gmail.com>
2021-03-31 22:27:23 -0300

    Add responsive header with theme toggler

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -16,25 +16,7 @@ export default function Header() {
 
     return (
         <header className="header-container">
-            <p style={{color: currentPalette.black}} onMouseOver={() => animateTitleText(true)} onMouseLeave={() => animateTitleText(false)}>
-                <span>{'<ef'}</span>
-                <span
-                    className="completed-name"
-                    style={{ width: fulltext ? '90px' : '0px', opacity: fulltext ? '1' : '0' }}>ilipe</span>
-                <span>{'x>'}</span>
-            </p>
             <div className="contacts-header">
-                <div onClick={() => toggleTheme(theme)} className="toggle-theme-container" style={{ background: currentPalette.tertiary }}>
-                    <input className="toggle-theme-button" type="checkbox" />
-                    <div className="index" style={{
-                        background: currentPalette.secondary,
-                        left: theme === 'light' ? 0 : 25
-                    }}>
-                        {
-                            theme === 'light' ? (<FiSun size={17} className="spin" color='#151515' />) : <FiMoon size={17} className="spin" color='#ddd' />
-                        }
-                    </div>
-                </div>
                 <a href="https://github.com/eFilipeCode" style={{ color: currentPalette.black }} target="_blank" rel="noreferrer">
                     <FiGithub size={25} />
                 </a>
@@ -44,6 +26,24 @@ export default function Header() {
                 <a href="mailto:efilipecode@gmail.com?Subject=Contacting via website" style={{ color: currentPalette.black }}>
                     <FiMail size={25} />
                 </a>
+            </div>
+            <p style={{ color: currentPalette.black }} onMouseOver={() => animateTitleText(true)} onMouseLeave={() => animateTitleText(false)}>
+                <span>{'<ef'}</span>
+                <span
+                    className="completed-name"
+                    style={{ width: fulltext ? '90px' : '0px', opacity: fulltext ? '1' : '0' }}>ilipe</span>
+                <span>{'x>'}</span>
+            </p>
+            <div onClick={() => toggleTheme(theme)} className="toggle-theme-container" style={{ background: currentPalette.tertiary }}>
+                <input className="toggle-theme-button" type="checkbox" />
+                <div className="index" style={{
+                    background: currentPalette.secondary,
+                    left: theme === 'light' ? 0 : 25
+                }}>
+                    {
+                        theme === 'light' ? (<FiSun size={17} className="spin" color='#151515' />) : <FiMoon size={17} className="spin" color='#ddd' />
+                    }
+                </div>
             </div>
         </header>
     );
