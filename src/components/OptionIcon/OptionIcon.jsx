423454885e1f604ed55edf38980508ce427aa489
Emanuel Filipe <efilipecode@gmail.com>
2021-04-01 22:06:43 -0300

    add lines to logo icons

--- src/components/OptionIcon/OptionIcon.jsx
+++ src/components/OptionIcon/OptionIcon.jsx
@@ -1,30 +1,26 @@
 import React, { useContext, useState } from 'react';
 import './styles.css';
 import { ThemeContext } from '../../contexts/ThemeContext';
+import { BsFillTriangleFill } from 'react-icons/bs'
 
 function OptionIcon({ isLinkToOtherPage, Icon, title, url, iconObjConfig, skillLevel }) {
     const { currentPalette } = useContext(ThemeContext);
     const [skillLevelState, setSkillLevel] = useState(0);
 
     function toggleSkillLevel(isMouseOver) {
-        isMouseOver ? setSkillLevel(skillLevel) : setSkillLevel(0);
+        isMouseOver === true ? setSkillLevel(skillLevel) : setSkillLevel(0);
     };
 
     return (
-        isLinkToOtherPage === true ?
-            (<a href={url} target="_blank" rel="noreferrer">
-                <div className="option-icon-container" onMouseOver={() => toggleSkillLevel(true)} onMouseLeave={() => toggleSkillLevel(false)}>
-                    <Icon {...iconObjConfig} />
-                    <p style={{ color: currentPalette.black }}>{title}</p>
-                    <div className="level-bar" style={{ width: `${skillLevel}%` }} />
+        <div className="option-icon-container" onMouseOver={() => toggleSkillLevel(true)} onMouseLeave={() => toggleSkillLevel(false)}>
+            <Icon color={currentPalette.black} {...iconObjConfig} />
+            <p style={{ color: currentPalette.black }}>{title}</p>
+            <div className="level-bar-container" style={{ background: skillLevelState !== 0 ? currentPalette.secondary : 'transparent' }}>
+                <div className="level-bar" style={{ width: skillLevelState !== 0 ? `${skillLevelState}%` : 0, background: iconObjConfig.color }}>
+                    <BsFillTriangleFill color={iconObjConfig.color} size={10} className="triangle" />
                 </div>
-            </a>)
-            :
-            (<div className="option-icon-container" onMouseOver={() => toggleSkillLevel(true)} onMouseLeave={() => toggleSkillLevel(false)}>
-                <Icon color={currentPalette.black} {...iconObjConfig} />
-                <p style={{ color: currentPalette.black }}>{title}</p>
-                <div className="level-bar" style={{ width: skillLevel !== 0 && `${skillLevelState}%`, background: iconObjConfig.color }} />
-            </div>)
+            </div>
+        </div>
     );
 };
 
