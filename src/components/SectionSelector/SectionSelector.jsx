b25d30c4af0a39d02d70f9720fcb4e81074cc9c6
Emanuel Filipe <efilipecode@gmail.com>
2021-04-04 16:39:17 -0300

    Add style to selected button

--- src/components/SectionSelector/SectionSelector.jsx
+++ src/components/SectionSelector/SectionSelector.jsx
@@ -1,37 +1,40 @@
-import { useContext } from 'react';
-import { WebsitesContext } from '../../contexts/WebsitesContext';
-import './SectionSelector.css';
-import websites from '../../utils/websites.json';
-import { ThemeContext } from '../../contexts/ThemeContext';
+import { WebsitesContext } from "../../contexts/WebsitesContext";
+import "./SectionSelector.css";
+import { useContext } from "react";
+import websites from "../../utils/websites.json";
+import { ThemeContext } from "../../contexts/ThemeContext";
 
 export default function SectionSelector() {
-    const {
-        setTitle,
-        setDescription,
-        setImage,
-        setLink,
-    } = useContext(WebsitesContext);
+  const { setTitle, setDescription, setImage, setLink, title } = useContext(
+    WebsitesContext
+  );
+  const { currentPalette } = useContext(ThemeContext);
 
-    const { currentPalette } = useContext(ThemeContext);
+  function handleChangeContextData({ title, link, imageUrl, description }) {
+    setTitle(title);
+    setDescription(description);
+    setImage(imageUrl);
+    setLink(link);
+  }
 
-    function handleChangeContextData({ title, link, imageUrl, description }) {
-        setTitle(title);
-        setDescription(description);
-        setImage(imageUrl);
-        setLink(link);
-    };
-
-    return (
-        <div className="sections-container">
-            <div className="project-buttons">
-                {[...websites].map(website => (
-                    <a href={`#${website.title}`}>
-                        <button style={{ borderColor: currentPalette.tertiary, color: currentPalette.black, background: currentPalette.secondary }} onClick={() => handleChangeContextData(website)}>
-                            {website.title}
-                        </button>
-                    </a>
-                ))}
-            </div>
-        </div>
-    );
-};
+  return (
+    <div className="sections-container">
+      <div className="project-buttons">
+        {[...websites].map((website) => (
+          <a href={`#${website.title}`}>
+            <button
+              style={{
+                borderColor: title === website.title ? '#10AC84' : currentPalette.tertiary,
+                color: title === website.title ? '#10AC84' : currentPalette.black,
+                background: currentPalette.secondary,
+              }}
+              onClick={() => handleChangeContextData(website)}
+            >
+              {website.title}
+            </button>
+          </a>
+        ))}
+      </div>
+    </div>
+  );
+}
